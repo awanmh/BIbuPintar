ca833c67547f4d1191804a8f2e07c3c69f018d3b
awanmh <setiawanmaulana6@gmail.com>
2025-07-24 00:07:40 +0700

    1.0

--- config/database.js
+++ config/database.js
@@ -23,14 +23,14 @@ if (process.env.DATABASE_URL) {
 } else {
   // Konfigurasi untuk development (lokal dengan MySQL dari .env)
   sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-      host: process.env.DB_HOST,
-      dialect: process.env.DB_DIALECT
-    }
-  );
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT || 'mysql' // fallback agar aman
+  }
+);
 }
 
 module.exports = sequelize;
